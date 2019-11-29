11d7a3cbc8a9e271de022340a4b2f1ae2eca29a9
shubham <shubhamgupta372@gmail.com>
2019-11-28 19:43:34 -0800

    minor changes

--- MCProject/predicttest.py
+++ MCProject/predicttest.py
@@ -1,6 +1,9 @@
+# sample code to test clarify API for detecting food objects from image
+
 from predict import getobjects
 
 if __name__=="__main__":
-   items=getobjects('images/test4.jpeg')
-   print items
+   items=getobjects('images/test5.jpeg')
+   print ("Objects Detected in image are")
+   print (items)
 
